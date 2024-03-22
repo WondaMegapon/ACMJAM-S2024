95507ff12545dcec8f7eabd9cecda381db396cb0
Wonda <wondamegapon@gmail.com>
2024-03-22 15:35:14 -0500

    Implementing Kinematic Body and Improved Movement

--- Scripts/Characters/Player.gd
+++ Scripts/Characters/Player.gd
@@ -1,23 +1,27 @@
-extends Node2D
+extends CharacterBody2D
 
-var moveSpeed = 60 # The speed of the character
-var jumpHeight = 3 # The impulse force of the character, when jumping.
-var dashLength = 5 # The impulse force of the character, when dashing.
-var gravity = -9.81 # The gravity of the character.
-var gravityCap = -100
-var drag = 0.85 # The inverse drag on the character.
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
 
-var momentum = Vector2(0, 0)
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta * (1 if Input.is_action_pressed("player_jump") else 2 )
 
+	# Handle jump.
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+		velocity.y += JUMP_VELOCITY
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	# momentum *= drag # Slowing the character down when there's no input.
-	var horizontalInput = Input.get_axis("player_left", "player_right") # Our input
-	momentum.x += horizontalInput * moveSpeed # Adding in our input.
-	#if(not transform.origin.y >= 200): momentum.y -= gravity
-	transform.origin += momentum * delta # Applying momentum.
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("player_left", "player_right")
+	
+	if Input.is_action_just_pressed("player_dash"):
+		velocity.x -= JUMP_VELOCITY * 2 * direction * (1.5 if is_on_floor() else 1)
+	
+	velocity.x = move_toward(velocity.x, direction * SPEED, 80)
+
+	move_and_slide()
