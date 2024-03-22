3a46f8d94c9c2874edeb4165875e288a87ee3afa
Wonda <wondamegapon@gmail.com>
2024-03-22 14:07:46 -0500

    Main scene and basic timer code implemented

---
+++ Scripts/GameMaster.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var globalTimer = 60.0 # The timer itself.
+
+enum GameState {MAIN_MENU, PAUSE_MENU, GAMEPLAY, CUTSCENE} # Tracking possible states.
+var currentState = GameState.GAMEPLAY # The current state.
+
+# For adding time to the timer.
+func globalTimerAdd(value: int):
+	globalTimer += value
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(globalTimer)
+	if(globalTimer <= 0):
+		print("I mad grrrr...")
+		globalTimerAdd(60)
+	
+	globalTimer -= delta # Subtracting time.
