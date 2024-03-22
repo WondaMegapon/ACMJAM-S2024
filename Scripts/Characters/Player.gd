42016ecfe0761c9f639dabde588ec5266cb0659b
Wonda <wondamegapon@gmail.com>
2024-03-22 15:53:33 -0500

    Implementing a cooldown on the dash.

--- Scripts/Characters/Player.gd
+++ Scripts/Characters/Player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -600.0
+const SPEED = 500.0 # The movement speed of the character.
+const JUMP_VELOCITY = -600.0 # The impulse jump force of the character.
+const DASH_MAX_COOLDOWN = 0.5 # The max cooldown of the dash
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var dashCooldown = 0 # The cooldown of the dash.
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,9 +21,16 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("player_left", "player_right")
 	
-	if Input.is_action_just_pressed("player_dash"):
+	# Handling the input gained from dashing.
+	if Input.is_action_just_pressed("player_dash") and dashCooldown <= 0:
 		velocity.x -= JUMP_VELOCITY * 2 * direction * (1.5 if is_on_floor() else 1)
+		dashCooldown = DASH_MAX_COOLDOWN # Resetting cooldown.
 	
+	# Drag on horizontal momentum.
 	velocity.x = move_toward(velocity.x, direction * SPEED, 100)
-
+	
+	# Decreasing the cooldown.
+	if(dashCooldown >= 0): dashCooldown -= delta
+	
+	# Moving~
 	move_and_slide()
