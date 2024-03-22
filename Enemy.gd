f5dfc0f8d24eca813977915578efe9b462696e65
Sheylo1 <wesley.austin-2@selu.edu>
2024-03-22 17:28:50 -0500

    hitboxes

--- Enemy.gd
+++ Enemy.gd
@@ -25,3 +25,10 @@ func _physics_process(delta):
 	velocity.x = direction * SPEED
 
 	move_and_slide()
+
+
+func _on_hurt_box_area_entered(hitbox):
+	var base_damage = hitbox.damage
+	self.hp -= base_damage
+	print(hitbox.get_parent().name + "'s hitbox touched " + name + "'s hurtbox and dealt " + str(base_damage))
+	
