d0de4e69ca9dfd7ea74fed06635d7f1753ceb562
Wonda <wondamegapon@gmail.com>
2024-03-22 16:07:02 -0500

    Adding iFrame variables and messing with viewport scaling.

--- Scripts/Characters/Player.gd
+++ Scripts/Characters/Player.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 const SPEED = 500.0 # The movement speed of the character.
 const JUMP_VELOCITY = -600.0 # The impulse jump force of the character.
 const DASH_MAX_COOLDOWN = 0.5 # The max cooldown of the dash
+const MAX_IFRAMES = 0.7 # The max iframes on the character.
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var dashCooldown = 0 # The cooldown of the dash.
+var iframes = 0 # 
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -23,7 +25,7 @@ func _physics_process(delta):
 	
 	# Handling the input gained from dashing.
 	if Input.is_action_just_pressed("player_dash") and dashCooldown <= 0:
-		velocity.x -= JUMP_VELOCITY * 2 * direction * (1.5 if is_on_floor() else 1)
+		velocity.x -= JUMP_VELOCITY * 2 * direction * (1.5 if is_on_floor() else 1.0)
 		dashCooldown = DASH_MAX_COOLDOWN # Resetting cooldown.
 	
 	# Drag on horizontal momentum.
