3e45cc81de41fd2e5c41ba419fd00db68ff2b2d0
Wonda <wondamegapon@gmail.com>
2024-03-22 21:15:14 -0500

    TIME ORB!!!

--- Scripts/Objects/TimeOrb.gd
+++ Scripts/Objects/TimeOrb.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
+@export var GravityArea = Area2D
 @export var CollectionArea = Area2D
+@onready var gameMaster = get_node("/root/GameMaster") as GameMaster # Getting our gamemaster.
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
 	if (CollectionArea as Area2D).has_overlapping_bodies():
 		var bodies = (CollectionArea as Area2D).get_overlapping_bodies()
-		transform.interpolate_with(bodies[0].transform, 0.1)
+		if(bodies[0]):
+			gameMaster.globalTimerAdd(2)
+			self.queue_free() # GONE
+	
+	if (GravityArea as Area2D).has_overlapping_bodies():
+		var bodies = (GravityArea as Area2D).get_overlapping_bodies()
+		if(bodies[0]):
+			position = position.move_toward(bodies[0].position, 64 / position.distance_to(bodies[0].position))
