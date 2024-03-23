8ad480217591cf60a51863284e8f1272261d2676
Wonda <wondamegapon@gmail.com>
2024-03-23 06:15:18 -0500

    Skull implemented.

---
+++ Scripts/Characters/Enemies/Skulljectile.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+
+@onready var gameMaster = get_node("/root/GameMaster") # Getting our gamemaster.
+
+var direction = 1  # 1 for right, -1 for left
+
+func _physics_process(delta):
+	if(gameMaster.currentState != GameMaster.GameState.GAMEPLAY): return
+
+	velocity.x = direction * SPEED
+	$AnimatedSprite2D.play("charge")
+	
+	var collisions = move_and_collide(velocity, true)
+	if(collisions):
+		queue_free()
+	
+	move_and_slide()
+
+func _process(delta):
+	($AnimatedSprite2D as AnimatedSprite2D).flip_h = direction == 1
+	
