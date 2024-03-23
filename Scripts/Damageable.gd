25b6b88d5b980f662a6375cd89ff4ebd45fb616c
Sheylo1 <wesley.austin-2@selu.edu>
2024-03-23 00:14:35 -0500

    EnemyKill = MoreTime

--- Scripts/Damageable.gd
+++ Scripts/Damageable.gd
@@ -1,9 +1,11 @@
 extends Node
 class_name Damageable
+@onready var gameMaster = get_node("/root/GameMaster") # Getting our gamemaster.
 @export var health : float = 2
 
 func hit(damage: int):
 	health -= damage
 	
-	if(health <= 0):
+	if health <= 0:
+		gameMaster.globalTimer += 3
 		get_parent().queue_free()
