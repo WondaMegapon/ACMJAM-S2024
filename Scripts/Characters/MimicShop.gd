da9b765bb7440d63460d81d0d60dc7289268f642
Sheylo1 <wesley.austin-2@selu.edu>
2024-03-23 00:42:47 -0500

    buy time

--- Scripts/Characters/MimicShop.gd
+++ Scripts/Characters/MimicShop.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
-
-@export var ShopArea = Area2D
 @onready var gameMaster = get_node("/root/GameMaster") as GameMaster # Getting our gamemaster.
+@export var ShopArea : Area2D
+var shopStock = 1 # Number of items in the shop
 
-# Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
-	if (ShopArea as Area2D).has_overlapping_bodies():
-		var bodies = (ShopArea as Area2D).get_overlapping_bodies()
-		print("In shop range")
+	if Input.is_action_pressed("player_interact"):
+		if ShopArea and (ShopArea as Area2D).has_overlapping_bodies():
+			interactWithShop()
+
+func interactWithShop():
+	print("In shop range")
+	if shopStock > 0:
+		print("Buying item from shop")
+		gameMaster.globalTimer += 30
+		shopStock -= 1
+		print("Item bought. Shop stock remaining:", shopStock)
+		if shopStock == 0:
+			print("Shop is out of stock")
